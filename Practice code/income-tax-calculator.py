20bbb09b6fa1117d6d022139fd3ea44046e21202
Sivajyothi <sivajyothi.aerospace@gmail.com>
2022-01-26 22:43:16 +0530

    Create income-tax-calculator.py
    Practice makes better:
    
    income tax calculator using python

---
+++ Practice code/income-tax-calculator.py
@@ -0,0 +1,10 @@
+"""
+ Problem statement: Income tax calculator
+
+ Input: Salary in Lakhs and regime (old/new)
+
+ Output: Tax deducted for that employee
+
+ income tax slab reference:
+ https://tax2win.in/guide/income-tax-slabs
+"""
